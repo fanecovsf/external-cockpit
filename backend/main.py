ad00a7d1c4906b30ec06057dfda5a3ee05d87ef5
fanecovsf <gustavo_h07@hotmail.com>
2026-04-13 13:03:59 -0300

    feat: initial config for ws

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,50 @@
 from fastapi import FastAPI, WebSocket
+from backend.schemas import CommandSchema, AutoPilotAltitudeSchema, TelemetryDataSchema
 
 app = FastAPI()
 
+command_schema = CommandSchema()
+auto_pilot_schema = AutoPilotAltitudeSchema()
+telemetry_schema = TelemetryDataSchema()
 
-@app.websocket("/ws")
+schemas = ['command', 'auto_pilot', 'telemetry']
+
+@app.websocket("/ws/cockpit")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_json()
-        print(data["command"])
-        print(data["status"])
-        await websocket.send_text(f"Echo: {data}")
+        try:
+            data = await websocket.receive_json()
+        except Exception as e:
+            print("Error receiving data:", e)
+            continue
+        
+        if 'schema' not in data:
+            continue
+
+        schema_name = data['schema']
+        if schema_name not in schemas:
+            continue
+
+        if schema_name == 'command':
+            try:
+                validated_data = command_schema.load(data)
+                print("Received Command:", validated_data)
+            except Exception as e:
+                print("Validation Error:", e)
+
+        elif schema_name == 'auto_pilot':
+            try:
+                validated_data = auto_pilot_schema.load(data)
+                print("Received AutoPilot Altitude:", validated_data)
+            except Exception as e:
+                print("Validation Error:", e)
+
+        elif schema_name == 'telemetry':
+            try:
+                validated_data = telemetry_schema.load(data)
+                print("Received Telemetry Data:", validated_data)
+            except Exception as e:
+                print("Validation Error:", e)
+        
+        
---
+++ backend/schemas.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields
+
+class CommandSchema(Schema):
+    schema = fields.Str(required=True)
+    command = fields.Str(required=True, validate=lambda x: x in ["AP", "TO", "LD", "FT"])
+    status = fields.Str(required=True, validate=lambda x: x in ["ON", "OFF"])
+
+class AutoPilotAltitudeSchema(Schema):
+    schema = fields.Str(required=True)
+    altitude = fields.Integer(required=True, validate=lambda x: 140 <= x <= 240)
+
+class TelemetryDataSchema(Schema):
+    schema = fields.Str(required=True)
+    altitude = fields.Integer(required=True)
+    speed = fields.Integer(required=True)
+    pitch = fields.Float(required=True)
+    roll = fields.Float(required=True)
